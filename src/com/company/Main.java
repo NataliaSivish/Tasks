69da1e995c16d8258e9065379676390c86a3be4c
Natasha <neta.tata@mail.ru>
2019-06-18 16:01:26 +0300

    2-исправлен метод

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,9 @@ import com.company.grep.text;
 public class Main {
 
     public static void main(String[] args) {
-        text t1 = new text("sdgfdhtrtd");
-        t1.textFiltering("shtsrh");
+        text t1 = new text("sd");
+        t1.characterOutputWord();
+        t1.textFiltering("jfhg sdfjjgh");
 
     }
 }
--- src/com/company/grep/text.java
+++ src/com/company/grep/text.java
@@ -19,10 +19,39 @@ public class text {
         this.word = word;
     }
 
-    public void textFiltering(String textFilter) {
+
+    public void characterOutputWord() {
         int size_word = word.length();
         for (int n = 0; n < size_word; n++) {
             System.out.println(word.charAt(n));
+        }
+
+    }
+
+    public void textFiltering(String textFilter) {
+        int size_word = word.length();
+        int m = 0;
+        int size_text = textFilter.length();
+        for (int i = 0; i < size_text; i++) {
+            i = m;
+            for (int j = 0; j < size_word; j++) {
+
+                if (word.charAt(j) == textFilter.charAt(i)) {
+                    i++;
+                    System.out.println("Слово - " );
+
+                    if (j == size_word - 1) {
+                        System.out.println("Слово - " + word + " есть в тексте:" + textFilter);
+                        break;
+                    }
+                }
+                else {
+                    i++;
+                    m = i;
+                    System.out.println("Слово - " );
+                }
+
+            }
         }
 
     }
